203a11ad336b154eff3ac9a74849e6cea7022396
shubhendu-goel <shubhendu.goel@interviewbit.com>
2020-09-09 13:38:51 +0530

    Creation and viewing of a task

---
+++ blog/app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+	def index
+		@tasks = Task.all
+	end
+	def show
+    @task = Task.find(params[:id])
+  	end
+ 
+  	def new
+  	end
+  	def edit
+  	end
+  	def create
+  		@task = Task.new(task_params)
+ 
+		@task.save
+		redirect_to @task
+  	end
+  	def update
+  	end
+  	def destory
+  	end
+  	private
+		def task_params
+    		params.require(:task).permit(:title, :text)
+		end	
+end
