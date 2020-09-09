23c8c09bcb77d08bd84129800ddc022eb08e843b
shubhendu-goel <shubhendu.goel@interviewbit.com>
2020-09-09 18:21:42 +0530

    Delete operation included

--- blog/app/controllers/tasks_controller.rb
+++ blog/app/controllers/tasks_controller.rb
@@ -10,6 +10,7 @@ class TasksController < ApplicationController
   		@task=Task.new
   	end
   	def edit
+  		@task = Task.find(params[:id])
   	end
   	def create
   		@task = Task.new(task_params)
@@ -21,8 +22,18 @@ class TasksController < ApplicationController
   		end
   	end
   	def update
+  		@task = Task.find(params[:id])
+		if @task.update(task_params)
+    		redirect_to @task
+		else
+			render 'edit'
+		end
   	end
-  	def destory
+  	def destroy
+  		@task = Task.find(params[:id])
+		@task.destroy
+
+		redirect_to tasks_path
   	end
   	private
 		def task_params
